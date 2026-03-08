ff4b55fe52779780a28e51ada96fa8aed070e439
Rsspades <lamosridhaan@gmail.com>
2026-03-08 10:31:47 -0400

    Create VariableExample.java
    Variable Practice Warm up

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariableExample.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+public class VariableExample {
+}
+//data types
+int robotHeight;
+double motorSpeed;
+boolean clawClosed;
+
+robotHeight=5
+motorSpeed=0.5
+clawClosed=true;
